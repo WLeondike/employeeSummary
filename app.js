18f190a14e0e2695121ff20a1721119f7444255f
WLeondike <billyleondike@gmail.com>
2020-07-08 16:46:10 -0400

    base function working, added comments :fire:

--- app.js
+++ app.js
@@ -1,133 +1,143 @@
-//GLOBALS
+//sub classes
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+//dependencies
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+//storing information
 const empArr = [];
+//creating the file
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
+//only one manager input
+let managerPresent = false;
 
-
-
-function letsGo() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "empOption",
-            message: "Please choose from the following the type of Employee you are:",
-            choices: [
-                "Manager",
-                "Engineer",
-                "Intern"
-            ]
+//main function -- choosing employee to add
+function addEmp() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "empOption",
+        message:
+          "Please choose from the following the type of Employee you'd like to add:",
+        choices: 
+        [
+            "Manager", 
+            "Engineer", 
+            "Intern", 
+            "I'm finished"
+        ]
+      },
+    ])
+    .then((answers) => {
+      if (answers.empOption === "Manager") {
+        if (managerPresent === false) {
+          managerPresent = true;
+          addManager();
+        } else {
+          console.log("We already have records of a Manager, press enter");
+          addEmp();
         }
-    ]).then(answers => {
-        
+      } else if (answers.empOption === "Engineer") {
+        addEngineer();
+      } else if (answers.empOption === "Intern") {
+        addIntern();
+      } else {
+        end();
+      }
     });
-};
+}
 
+//manager details
 function addManager() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "mName",
-            message: "Name of Manager?"
-        },
-        {
-            type: "input",
-            name: "mID",
-            message: "Manager's ID?"
-        },
-        {
-            type: "input",
-            name: "mEmail",
-            message: "Manager's email?"
-        },
-        {
-            type: "input",
-            name: "mOfficeNumber",
-            message: "Office number for the Manager?"
-        }
-    ]).then(answers => {
-        
-    });
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "mName",
+        message: "Name of Manager?",
+      },
+      {
+        type: "input",
+        name: "mID",
+        message: "Manager's ID?",
+      },
+      {
+        type: "input",
+        name: "mEmail",
+        message: "Manager's email?",
+      },
+      {
+        type: "input",
+        name: "mOfficeNumber",
+        message: "Office number for the Manager?",
+      },
+    ])
+    .then((answers) => {});
+}
+
+//engineer details
 function addEngineer() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "eName",
-            message: "Name of Engineer"
-        },
-        {
-            type: "input",
-            name: "eID",
-            message: "Engineer's ID?"
-        },
-        {
-            type: "input",
-            name: "eEmail",
-            message: "Engineer's email?"
-        },
-        {
-            type: "input",
-            name: "eGitHub",
-            message: "gitHub username for the Engineer?"
-        }
-    ]).then(answers => {
-        
-    });
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "eName",
+        message: "Name of Engineer",
+      },
+      {
+        type: "input",
+        name: "eID",
+        message: "Engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "eEmail",
+        message: "Engineer's email?",
+      },
+      {
+        type: "input",
+        name: "eGitHub",
+        message: "gitHub username for the Engineer?",
+      },
+    ])
+    .then((answers) => {});
+}
+
+//intern details
 function addIntern() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "iName",
-            message: "Name of Intern"
-        },
-        {
-            type: "input",
-            name: "iID",
-            message: "Intern's ID?"
-        },
-        {
-            type: "input",
-            name: "iEmail",
-            message: "Intern's email?"
-        },
-        {
-            type: "input",
-            name: "iSchool",
-            message: "What school did the Intern graduate from?"
-        }
-    ]).then(answers => {
-        
-    });
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "iName",
+        message: "Name of Intern",
+      },
+      {
+        type: "input",
+        name: "iID",
+        message: "Intern's ID?",
+      },
+      {
+        type: "input",
+        name: "iEmail",
+        message: "Intern's email?",
+      },
+      {
+        type: "input",
+        name: "iSchool",
+        message: "What school did the Intern graduate from?",
+      },
+    ])
+    .then((answers) => {});
+}
+addEmp();
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//generate file
+function end() {
+  console.log("Employee Summary file created!");
+}
